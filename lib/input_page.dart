251cc71fb2f092d7697af4a6165b13c19ada8fe0
Vincent Tombari <vinnietombari@gmail.com>
2021-08-17 10:39:41 -0500

    Lesson 4 complete

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -20,8 +22,24 @@ class _InputPageState extends State<InputPage> {
       body: Column(children: [
         Expanded(
           child: Row(children: [
-            Expanded(child: ReusableCard(colour: activeCardColor)),
-            Expanded(child: ReusableCard(colour: activeCardColor)),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColor,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.mars,
+                  label: 'MALE',
+                ),
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColor,
+                cardChild: IconContent(
+                  icon: FontAwesomeIcons.venus,
+                  label: 'FEMALE',
+                ),
+              ),
+            ),
           ]),
         ),
         Expanded(
@@ -42,23 +60,6 @@ class _InputPageState extends State<InputPage> {
           height: bottomContainerHeight,
         ),
       ]),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
-
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
     );
   }
 }
