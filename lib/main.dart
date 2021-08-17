0d7dfbba3a211ab70cb78367a5eb354e6615f6a2
Vincent Tombari <vinnietombari@gmail.com>
2021-08-16 21:36:54 -0500

    Lesson 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xff0a0d22),
-        scaffoldBackgroundColor: Color(0xFF070a1a),
+        primaryColor: Color(0xff0A0D22),
+        scaffoldBackgroundColor: Color(0xFF070A1A),
       ),
       home: InputPage(),
     );
